aa4a3e373dc0a609c9c77c9f3437f566cefc4c0a
E. G. Patrick Bos <egpbos@gmail.com>
2020-09-23 14:00:34 +0200

    make this a pip installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+from setuptools import setup
+
+setup(name='flickr1d',
+      version='0.1',
+      description='A mini version of the Flickr8k written and spoken caption corpora',
+      url='https://github.com/spokenlanguage/flickr1d',
+      author='Patrick Bos',
+      author_email='p.bos@esciencecenter.nl',
+      zip_safe=False,
+      packages=['flickr1d'],
+      include_package_data=True,
+      )
